3997001e35b847c108f68ff121c5e87c0c635095
shunko7 <shunko7@users.noreply.github.com>
2017-06-20 10:48:28 +0200

    php fix

---
+++ configs/views/Startsite.php
@@ -0,0 +1,33 @@
+<?php echo $this->header; ?>
+
+<header>
+    <div class="header">
+        <h1 class="h1header">Studenten-Gathering </h1>
+        <h4 class="h4header">FH Kufstein</h4>
+    </div>
+</header>
+
+<div class="row">
+    <div class="picture first" >
+        <div class="bildschrift">
+            <h2>Wohnen</h2>
+        </div>
+    </div>
+    <div class="picture second">
+        <div class="bildschrift">
+            <h2>Nachhilfe</h2>
+        </div>
+    </div>
+    <div class="picture third" >
+        <div class="bildschrift">
+            <h2>Veranstaltungen</h2>
+        </div>
+    </div>
+    <div class="picture fourth">
+        <div class="bildschrift">
+            <h2>Auslandssemester</h2>
+        </div>
+    </div>
+
+</div>
+<?php echo $this->footer; ?>
