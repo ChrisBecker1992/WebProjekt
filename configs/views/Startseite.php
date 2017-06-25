7b76c3693bd0f71234c465f3cb037e2cb88da9bf
Christopher <Christopher>
2017-06-25 13:27:33 +0200

    Bilder mit Link versehen

--- configs/views/Startseite.php
+++ configs/views/Startseite.php
@@ -34,21 +34,25 @@
     <div class="picture first" >
         <div class="bildschrift">
             <h2>Wohnen</h2>
+            <a href="Wohnen"></a>
         </div>
     </div>
     <div class="picture second">
         <div class="bildschrift">
             <h2>Nachhilfe</h2>
+            <a href="Nachhilfe"></a>
         </div>
     </div>
     <div class="picture third" >
         <div class="bildschrift">
             <h2>Veranstaltungen</h2>
+            <a href="Veranstaltungen"></a>
         </div>
     </div>
     <div class="picture fourth">
         <div class="bildschrift">
             <h2>Auslandssemester</h2>
+            <a href="Auslandssemester"></a>
         </div>
     </div>
 
