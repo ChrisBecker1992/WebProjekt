7e5974326eb9f3dfd514f15267296ce35ef5977b
shunko7 <shunko7@users.noreply.github.com>
2017-06-25 14:03:09 +0200

    forgot reference..

--- configs/views/Startseite.php
+++ configs/views/Startseite.php
@@ -9,6 +9,16 @@
 
     <!-- Dropdown-Menü -->
 <div class="menu">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="logout">(Abmelden)</a></li>
+        </ul>
+
+        <p class="navbar-text navbar-right">Angemeldet als <strong class="username"><?php echo $this->username; ?></strong></p>
+
+    </div>
     <div>
         <div class="dropdown">
             <div class="container" onclick="myFunction(this)">
