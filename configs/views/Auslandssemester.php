3f97d99d2876b04d13620bbeb5284b6bd2858d95
shunko7 <shunko7@users.noreply.github.com>
2017-06-25 20:21:38 +0200

    trying to solve insert

--- configs/views/Auslandssemester.php
+++ configs/views/Auslandssemester.php
@@ -4,30 +4,60 @@
 <br><br>
 
 <!--Button für neuen Eintrag-->
+<div id="main">
+    <div class="row">
 
-<div class="links col-xs-10"><h1>Auslandssemester</h1></div>
-<div class="rechts col-xs-2">
-    <button class="btn btn-lg btn-primary btn-block beitragbutton">+ neuer Beitrag</button>
+        <div class="links col-xs-10"><h1>Auslandssemester</h1></div>
+        <div class="rechts col-xs-2">
+        <button class="btn btn-lg btn-primary btn-block beitragbutton">+ neuer Beitrag</button>
+        </div>
+
+        <?php if($this->ausland): ?>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Beitrag</th>
+                    <th>Bearbeiten</th>
+                    <th>Löschen</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach($this->ausland as $ausland): ?>
+                    <tr>
+                        <td><?php echo $ausland->id; ?></td>
+                        <td><?php echo $ausland->topic; ?></td>
+
+                        <td><button class="btn btn-default" data-toggle="modal" data-target="#editModal" data-id="<?php echo $ausland->id; ?>"><i class="glyphicon glyphicon-pencil"></i> Bearbeiten</button></td>
+                        <td><a class="btn btn-danger triggerDelete" href="api/address/" data-id="<?php echo $ausland->id; ?>"><i class="glyphicon glyphicon-trash"></i> Löschen</a></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p>&nbsp;</p>
+            <div class="alert alert-info">Noch keine Auslandserfahrungen vorhanden - Sie können über den Button <strong>neuer Beitrag</strong> eine neue Erfrahrung hinzufügen.</div>
+        <?php endif; ?>
+    </div>
 </div>
-
 <br><br>
 <br><br>
 
 
-<!-- Textfeld für Eintrag -->
+<!-- <!-- Textfeld für Eintrag
 <div class="links col-xs-1"></div>
 
 <textarea class="innen col-xs-8" cols="100" rows="10">
 </textarea>
 
 <div class="rechts col-xs-3"></div>
-
+-->
 <br><br>
 
 
 <!--Lightbox für neuen Eintrag-->
 
-<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -39,14 +69,22 @@
 
                 <form class="Beitrag">
 
-                    <label for="Betreff" class="sr-only"></label>
-                    <input type="text" id="Betreff" class="form-control" placeholder="neuer Betreff" required autofocus>
+                    <!--<label for="Betreff" class="sr-only"></label>
+                    <input type="text" id="Betreff" class="form-control" placeholder="neuer Betreff" required autofocus>-->
+
+                    <div class="form-group">
+                        <label for="topic">Beitrag</label>
+                        <input type="text" name="topic" class="form-control" id="topic" value="<?php echo $this->topic; ?>">
+                    </div>
 
                     <br><br>
 
-
-
-                    <button type="button" class="btn btn-primary registrieren">Speichern</button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+                <button type="button" class="btn btn-primary registrieren">Speichern</button>
+            </div>
+            
 
                 </form><!-- /form -->
 
@@ -54,7 +92,70 @@
         </div>
     </div>
 </div>
+    <script type="text/javascript">
 
+        var editModal = $('#editModal');
+
+        editModal.find('form').unbind('submit');
+
+        editModal.find('form').bind('submit', function(e, that) {
+
+            e.preventDefault();
+
+            editModal.find('.btn-primary').prop('disabled', true);
+
+            hasError = false;
+
+            if(typeof that === 'undefined') {
+                that = editModal.find('.btn-primary').get(0);
+            }
+
+            var requiredFields = ['#topic'];
+
+            for(var i = 0; i < requiredFields.length; i++) {
+                if($(requiredFields[i]).val() == '') {
+                    hasError = true;
+                    $(requiredFields[i]).closest('.form-group').addClass('has-error');
+                }
+            }
+
+            if(!hasError)
+            {
+                $.ajax({
+                    'url': $(this).attr('action'),
+                    'method': $(this).attr('method'),
+                    'data': $(this).serialize(),
+                    'dataType': "json",
+                    'success': function (receivedData) {
+
+                        if(receivedData.result)
+                        {
+                            window.setTimeout(function() {
+                                location.reload();
+                            }, 500);
+
+                        }
+                        else
+                        {
+                            editModal.find('.form-group').removeClass('has-error');
+
+                            $.each(receivedData.data.errorFields, function(key, value) {
+                                $('#'+key).closest('.form-group').addClass('has-error');
+                            });
+                        }
+
+                        editModal.find('.btn-primary').prop('disabled', false);
+                    }
+                });
+            }
+            else
+            {
+                editModal.find('.btn-primary').prop('disabled', false);
+            }
+
+        });
+
+    </script>
 
     <footer>
         <div class="socialmedia">
@@ -67,6 +168,5 @@
             </a>
         </div>
     </footer>
-
 
 <?php echo $this->footer; ?>
